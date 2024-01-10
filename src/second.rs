083c5bb04058c05eb62b94b53fde9c0dcae328f9
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-01-10 16:26:13 +1000

    deref()

--- src/second.rs
+++ src/second.rs
@@ -52,8 +52,8 @@ impl<T> List<T> {
     IntoIter(self)
   }
 
-  pub fn iter<'a>(&'a self) -> Iter<'a, T> {
-    Iter { next: self.head.map(|node| &node) }
+  pub fn iter(&self) -> Iter<'_, T> {
+    Iter { next: self.head.as_deref() }
   }
 
 }
@@ -79,7 +79,7 @@ impl<'a, T> Iterator for Iter<'a, T> {
   type Item = &'a T;
   fn next(&mut self) -> Option<Self::Item> {
     self.next.map(|node| {
-      self.next = node.next.map(|node| &node);
+      self.next = node.next.as_deref();
       &node.elem
     })
   }
@@ -145,6 +145,17 @@ mod test {
     assert_eq!(iter.next(), Some(2));
     assert_eq!(iter.next(), Some(1));
     assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
   }
 
 }
